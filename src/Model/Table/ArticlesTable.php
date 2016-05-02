31dc57997939f5a059b6f73ceb83c83d7b4faa71
ashik <ashik.kalavadiya@gmail.com>
2016-05-02 15:16:53 +0530

    Initail Commit

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Article;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Articles Model
+ *
+ */
+class ArticlesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('articles');
+        $this->displayField('title');
+        $this->primaryKey('id');
+         $this->addBehavior('Timestamp');
+        // Just add the belongsTo relation with CategoriesTable
+        $this->belongsTo('Categories', [
+            'foreignKey' => 'category_id',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->notEmpty('title')
+            ->requirePresence('title')
+            ->notEmpty('body')
+            ->requirePresence('body');
+
+        return $validator;
+    }
+    
+    public function isOwnedBy($articleId, $userId)
+    {
+        return $this->exists(['id' => $articleId, 'user_id' => $userId]);
+    }
+}
